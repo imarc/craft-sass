8422dfcb5dcaf736694b9460ff8746f74e0609a2
Jeff Turcotte <jeff.turcotte@gmail.com>
2016-05-18 12:26:15 -0400

    Fixed tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,20 @@
 <?php
 namespace Craft;
 
+// do not bootstrap a second time for
+// tests that are run in their own process
+if (defined('ROOT')) {
+    return;
+}
+
+define('ROOT', dirname(__DIR__));
+
+include ROOT . '/vendor/autoload.php';
+include ROOT . '/services/Sass_CompilerService.php';
+include ROOT . '/variables/SassVariable.php';
+
+// some dead simple mocks
+
 class BaseApplicationComponent {}
 
 class Request {
@@ -9,19 +23,28 @@ class Request {
     }
 }
 
-define('ROOT', dirname(__DIR__));
+class IOHelper {
+    static function ensureFolderExists($dir)
+    {
+        if (!is_dir($dir)) {
+            throw new \Exception('Dir doesn\'t exist');
+        }
+    }
+}
 
-include ROOT . '/services/Sass_CompilerService.php';
-include ROOT . '/variables/SassVariable.php';
-
+class Path {
+    function getStoragePath() {
+        return __DIR__ . '/storage';
+    }
+}
 
 $craft = (object) [
     'sass_compiler' => new Sass_CompilerService(),
-    'request' => new Request()
+	'request' => new Request(),
+	'path' => new Path()
 ];
 
 function craft() {
     global $craft;
     return $craft;
 }
-
